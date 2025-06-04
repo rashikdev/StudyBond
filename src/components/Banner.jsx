c28528d56a79e963126a4f8975fa6906769b9e68
rashik513 <mdrobiulislamrashikpdf@gmail.com>
2025-06-04 20:31:42 +0600

    add banner section

---
+++ src/components/Banner.jsx
@@ -0,0 +1,179 @@
+import React from "react";
+import animationBg from "../assets/animationbg.json";
+import Lottie from "lottie-react";
+import { motion } from "motion/react";
+import {
+  FaFacebook,
+  FaLinkedinIn,
+  FaTelegramPlane,
+  FaTwitter,
+} from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io";
+const Banner = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+  const itemVariants = {
+    hidden: { opacity: 0, x: 100 },
+    hover: { scale: 1.2 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      scale: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <div className="relative">
+      <Lottie
+        animationData={animationBg}
+        loop={true}
+        autoPlay={true}
+        className="h-[90vh] "
+      />
+      <div className="h-[90vh] w-full absolute top-0 left-0 bg-transparent">
+        <div>
+          <div className="absolute top-40 left-10 hidden md:flex">
+            <motion.ul
+              variants={containerVariants}
+              initial="hidden"
+              animate="visible"
+              className="flex flex-col gap-4"
+            >
+              <motion.a
+                href=""
+                variants={itemVariants}
+                className="bg-zinc-700 p-[6px] rounded-full border-[1.5px] border-[#37c386]"
+              >
+                <FaLinkedinIn size={22} color="white" />
+              </motion.a>
+              <motion.a
+                href=""
+                variants={itemVariants}
+                className="bg-zinc-700 p-[6px] rounded-full border-[1.5px] border-[#37c386]"
+              >
+                <FaFacebook size={24} color="white" />
+              </motion.a>
+              <motion.a
+                href=""
+                variants={itemVariants}
+                className="bg-zinc-700 p-[6px] rounded-full border-[1.5px] border-[#37c386]"
+              >
+                <IoLogoInstagram size={24} color="white" />
+              </motion.a>
+              <motion.a
+                href=""
+                variants={itemVariants}
+                className="bg-zinc-700 p-[6px] rounded-full border-[1.5px] border-[#37c386]"
+              >
+                <FaTwitter size={22} color="white" />
+              </motion.a>
+              <motion.a
+                href=""
+                variants={itemVariants}
+                className="bg-zinc-700 p-[6px] rounded-full border-[1.5px] border-[#37c386]"
+              >
+                <FaTelegramPlane size={24} color="white" />
+              </motion.a>
+            </motion.ul>
+          </div>
+        </div>
+        {/* for Small device  */}
+        <div className="absolute bottom-5 left-2 md:hidden">
+          <h2
+            className="text-2xl mb-3
+          font-thin"
+          >
+            Social Contact
+          </h2>
+          <motion.ul
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            className="flex gap-3"
+          >
+            <motion.a
+              href=""
+              variants={itemVariants}
+              className="bg-zinc-700 p-[6px] rounded-full"
+            >
+              <FaLinkedinIn size={12} />
+            </motion.a>
+            <motion.a
+              href=""
+              variants={itemVariants}
+              className="bg-zinc-700 p-[6px] rounded-full"
+            >
+              <FaFacebook size={14} />
+            </motion.a>
+            <motion.a
+              href=""
+              variants={itemVariants}
+              className="bg-zinc-700 p-[6px] rounded-full"
+            >
+              <IoLogoInstagram size={14} />
+            </motion.a>
+            <motion.a
+              href=""
+              variants={itemVariants}
+              className="bg-zinc-700 p-[6px] rounded-full"
+            >
+              <FaTwitter size={12} />
+            </motion.a>
+            <motion.a
+              href=""
+              variants={itemVariants}
+              className="bg-zinc-700 p-[6px] rounded-full"
+            >
+              <FaTelegramPlane size={14} />
+            </motion.a>
+          </motion.ul>
+        </div>
+        <div className="absolute bottom-30 left-10 space-y-10">
+          <motion.h1
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 2, delay: 0.2, ease: "backInOut" }}
+            className="text-5xl font-bold"
+          >
+            Study Together. Grow Smarter.
+          </motion.h1>
+          <motion.p
+            className="text-lg text-center md:text-left"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+          >
+            Join{" "}
+            <span className="uppercase font-thin underline underline-offset-4 text-green-300">
+              Study Bond
+            </span>{" "}
+            – a collaborative platform <br className="hidden md:block" />
+            where friends share, submit, and grade assignments together.
+          </motion.p>
+          <motion.button
+            className="border px-6 py-2 rounded-full transition-all duration-200 cursor-pointer bg-white/20 backdrop-blur-[2px]"
+            initial={{ opacity: 0, scale: 0.5, x: 30 }}
+            animate={{ opacity: 1, scale: 1, x: 0 }}
+            transition={{ duration: 1.5, ease: "backOut", delay: 0.4 }}
+            whileHover={{ scale: 1.1, transition: { duration: 0.1 } }}
+            whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
+          >
+            Get Started
+          </motion.button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,127 +23,129 @@ const Navbar = () => {
       });
   };
   return (
-    <div className="flex justify-between items-center md:py-3 py-1 md:px-7 px-4 border rounded-full mt-10 relative">
-      <button onClick={handleMenu} className="md:hidden">
-        <Hamburger size={26}></Hamburger>
-      </button>
-      {isOpen && (
-        <div className="w-30 h-[30vh] bg-white/10 backdrop-blur-lg absolute left-0 top-20">
-          <ul className="flex flex-col gap-1 p-3 text-[10px]">
+    <div className="fixed top-0 left-0 w-full z-50">
+      <div className="flex justify-between items-center md:py-3 py-1 md:px-7 px-4 border rounded-full md:mt-10 mt-5 relative mx-2 md:mx-0 backdrop-blur-md">
+        <button onClick={handleMenu} className="md:hidden">
+          <Hamburger size={26}></Hamburger>
+        </button>
+        {isOpen && (
+          <div className="w-30 h-[30vh] bg-white/10 backdrop-blur-lg absolute left-0 top-20">
+            <ul className="flex flex-col gap-1 p-3 text-[10px]">
+              <NavLink
+                to="/"
+                onClick={() => setOpen(false)}
+                className={({ isActive }) => (isActive ? "" : "")}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/assignments"
+                onClick={() => setOpen(false)}
+                className={({ isActive }) => (isActive ? "" : "")}
+              >
+                Assignments
+              </NavLink>
+              {user && (
+                <>
+                  <NavLink
+                    to="/pending-assignments"
+                    onClick={() => setOpen(false)}
+                    className={({ isActive }) => (isActive ? "" : "")}
+                  >
+                    Pending Assignments
+                  </NavLink>
+                  <NavLink
+                    to="/dashboard"
+                    onClick={() => setOpen(false)}
+                    className={({ isActive }) => (isActive ? "" : "")}
+                  >
+                    Dashboard
+                  </NavLink>
+                </>
+              )}
+            </ul>
+          </div>
+        )}
+        <h1 className="md:text-2xl font-semibold uppercase">Study Bond</h1>
+        <div>
+          <ul className="gap-6 hidden md:flex items-center">
             <NavLink
               to="/"
-              onClick={() => setOpen(false)}
-              className={({ isActive }) => (isActive ? "" : "")}
+              className={({ isActive }) =>
+                isActive ? "border-2 rounded-full px-4 py-1" : ""
+              }
             >
               Home
             </NavLink>
             <NavLink
               to="/assignments"
-              onClick={() => setOpen(false)}
-              className={({ isActive }) => (isActive ? "" : "")}
+              className={({ isActive }) =>
+                isActive ? "border-2 rounded-full px-4 py-1" : ""
+              }
             >
               Assignments
             </NavLink>
-            {user && (
+            {
               <>
                 <NavLink
                   to="/pending-assignments"
-                  onClick={() => setOpen(false)}
-                  className={({ isActive }) => (isActive ? "" : "")}
+                  className={({ isActive }) =>
+                    isActive ? "border-2 rounded-full px-4 py-1" : ""
+                  }
                 >
                   Pending Assignments
                 </NavLink>
-                <NavLink
-                  to="/dashboard"
-                  onClick={() => setOpen(false)}
-                  className={({ isActive }) => (isActive ? "" : "")}
-                >
-                  Dashboard
-                </NavLink>
               </>
-            )}
+            }
           </ul>
         </div>
-      )}
-      <h1 className="md:text-2xl font-semibold uppercase">Study Bond</h1>
-      <div>
-        <ul className="gap-6 hidden md:flex items-center">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? "border-2 rounded-full px-4 py-1" : ""
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/assignments"
-            className={({ isActive }) =>
-              isActive ? "border-2 rounded-full px-4 py-1" : ""
-            }
-          >
-            Assignments
-          </NavLink>
-          {
-            <>
-              <NavLink
-                to="/pending-assignments"
-                className={({ isActive }) =>
-                  isActive ? "border-2 rounded-full px-4 py-1" : ""
-                }
-              >
-                Pending Assignments
-              </NavLink>
-            </>
-          }
-        </ul>
-      </div>
-      <div className="absolute hidden lg:block right-70 border-r-2 border-l-2 rounded-full px-4 py-1 hover:scale-105 cursor-pointer transition-all duration-300">
-        <NavLink>Dasheboard</NavLink>
-      </div>
-      <div>
-        <div className="flex items-center gap-5">
-          {user && (
-            <img
-              onClick={() => setShow(!show)}
-              title={user.displayName}
-              className="w-10 h-10 rounded-full border-2 cursor-pointer"
-              src={user?.photoURL ? user.photoURL : userLogo}
-              alt={user.displayName}
-            />
-          )}
-          <div>
-            {user ? (
-              <Link to="/login">
-                <button
-                  onClick={handleLogout}
-                  className="border-2 text-red-500 rounded-full px-4 py-1 cursor-pointer"
-                >
-                  Logout
-                </button>
-              </Link>
-            ) : (
-              <Link to="/login">
-                <button className="border-2 rounded-full px-4 py-1 cursor-pointer">
-                  Login
-                </button>
-              </Link>
+        <div className="absolute hidden lg:block right-70 border-r-2 border-l-2 rounded-full px-4 py-1 hover:scale-105 cursor-pointer transition-all duration-300">
+          <NavLink>Dasheboard</NavLink>
+        </div>
+        <div>
+          <div className="flex items-center gap-5">
+            {user && (
+              <img
+                onClick={() => setShow(!show)}
+                title={user.displayName}
+                className="w-10 h-10 rounded-full border-2 cursor-pointer"
+                src={user?.photoURL ? user.photoURL : userLogo}
+                alt={user.displayName}
+              />
             )}
+            <div>
+              {user ? (
+                <Link to="/login">
+                  <button
+                    onClick={handleLogout}
+                    className="border-2 text-red-500 rounded-full px-4 py-1 cursor-pointer"
+                  >
+                    Logout
+                  </button>
+                </Link>
+              ) : (
+                <Link to="/login">
+                  <button className="border-2 rounded-full px-4 py-1 cursor-pointer">
+                    Login
+                  </button>
+                </Link>
+              )}
+            </div>
           </div>
         </div>
+        {show && (
+          <div className="absolute top-20 right-5 bg-white/20 p-2 rounded">
+            <ul className="flex flex-col gap-1">
+              <Link onClick={() => setShow(false)}>
+                <li>Create Assignment</li>
+              </Link>
+              <Link onClick={() => setShow(false)}>
+                <li>My Attempted Assignments</li>
+              </Link>
+            </ul>
+          </div>
+        )}
       </div>
-      {show && (
-        <div className="absolute top-20 right-5 bg-white/20 p-2 rounded">
-          <ul className="flex flex-col gap-1">
-            <Link onClick={() => setShow(false)}>
-              <li>Create Assignment</li>
-            </Link>
-            <Link onClick={() => setShow(false)}>
-              <li>My Attempted Assignments</li>
-            </Link>
-          </ul>
-        </div>
-      )}
     </div>
   );
 };
